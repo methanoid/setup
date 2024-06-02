121f7cb89a942c3f30379ad9d4b6e6b942eb6d13
methanoid <methanoid@gmail.com>
2024-06-02 13:52:40 +0000

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-
 # DOES NOT WORK... Fed40 & Plasma 6 with Wayland is BROKEN..DONT!!! You need X11
-
 # SETUP SCRIPT FOR FEDORA BASED DISTROS USING DNF
 
 # echo "Installing RPM Fusion..."
-sudo dnf install -y "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${os_version}.noarch.rpm" "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${os_version}.noarch.rpm"
-sudo dnf update -y # and reboot if you are not on the latest kernel
-
-
+# sudo dnf install -y "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${os_version}.noarch.rpm" "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${os_version}.noarch.rpm"
+# sudo dnf update -y # and reboot if you are not on the latest kernel
 
 nvgpu=$(lspci | grep -i nvidia | grep -i vga | cut -d ":" -f 3)
 if [[ $nvgpu ]]; then
@@ -19,9 +15,7 @@ fi
 #  printf 'Installing AMD GPU bits:  %s\n'
 #  printf "RADV_PERFTEST=aco" >> /etc/environment
 #fi
-
-# POWER TUNING
-# hostnamectl set-hostname new-name
+hostnamectl set-hostname new-name
 
 # BRAVE
 #sudo systemctl disable NetworkManager-wait-online.service  # Think this is just for Ubuntu derivatives.  sudo systemd-analyze critical-chain indicates they waste 5s on boot
@@ -39,8 +33,8 @@ sudo flatpak install -y flathub com.heroicgameslauncher.hgl
 sudo dnf install -y bottles lutris dosbox-staging neofetch bleachbit python3-pip mc git ncdu gamemode hunspell-en-GB vlc powertop steam steam-devices curl retroarch
 sudo dnf remove -y plasma-welcome ktorrent firefox neochat skanpage kmahjongg
 sudo dnf upgrade -y && sudo dnf autoremove -y
-#sudo printf "neofetch" >> /home/$USER/.bashrc
-#sudo timedatectl set-local-rtc 1 --adjust-system-clock
+sudo printf "neofetch" >> /home/$USER/.bashrc
+sudo timedatectl set-local-rtc 1 --adjust-system-clock
 
 #wget https://github.com/methanoid/setup/blob/main/powertuning.sh
 #sudo sh ./powertuning.sh
