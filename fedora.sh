43e492d4bb88e65aedf969e3a0e344c298588e35
methanoid <methanoid@gmail.com>
2025-04-25 10:05:39 +0100

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -11,14 +11,10 @@ nvgpu=$(lspci | grep -i nvidia | grep -i vga | cut -d ":" -f 3)
 if [[ $nvgpu ]]; then
   sudo -S dnf install -y akmod-nvidia # xorg-x11-drv-nvidia-cuda gwe
 fi
-#else
-#  printf "RADV_PERFTEST=aco" >> /etc/environment
-#fi
-
-# BRAVE
-sudo dnf install -y dnf-plugins-core && sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo && sudo dnf install -y brave-browser
+#else    #  printf "RADV_PERFTEST=aco" >> /etc/environment   #fi
 
 #GENERAL INSTALLS
+sudo dnf install -y dnf-plugins-core && sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo && sudo dnf install -y brave-browser
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo && sudo flatpak remote-modify --no-filter --enable flathub && sudo flatpak remote-delete fedora
 sudo flatpak install -y --noninteractive --system org.gtk.Gtk3theme.Breeze
 sudo flatpak override --filesystem=xdg-config/gtk-3.0:ro 
@@ -36,6 +32,6 @@ sudo flatpak install -y flathub net.retrodeck.retrodeck
 sudo dnf up -y && sudo dnf autoremove -y
 
 ## sudo dnf remove -y plasma-welcome ktorrent firefox neochat skanpage kmahjongg
-## sudo timedatectl set-local-rtc 1 --adjust-system-clock
-# wget https://github.com/methanoid/setup/blob/main/powertuning.sh && sudo sh ./powertuning.sh
-# CONF files, # make konsole & BRAVE favouriteS, # screen locking,sleep settings, # proton-ge / proton up ??
+## sudo timedatectl set-local-rtc 1 --adjust-system-clock    # For MultiBoot systems
+## wget https://github.com/methanoid/setup/blob/main/powertuning.sh && sudo sh ./powertuning.sh
+## CONF files, # make konsole & BRAVE favouriteS, # screen locking,sleep settings, # proton-ge / proton up ??
