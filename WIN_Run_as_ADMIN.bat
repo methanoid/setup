935ab2e5de468913772f7e2254c8ab17503febdf
methanoid <methanoid@gmail.com>
2025-07-23 16:49:33 +0100

    Update WIN_Run_as_ADMIN.bat
    Dont remove Edge :-(

--- WIN_Run_as_ADMIN.bat
+++ WIN_Run_as_ADMIN.bat
@@ -77,15 +77,15 @@ wuauclt /updatenow
 :: ==INSTALLS========================================================================================================================
 
 title Debloating Last AppX
-Remove-AppXProvisionedPackage -Online -PackageName Microsoft.MicrosoftEdgeDevToolsClient* -AllUsers
-Remove-AppXProvisionedPackage -Online -PackageName Microsoft.Edge.GameAssist* -AllUsers
-Remove-AppXProvisionedPackage -Online -PackageName Microsoft.MicrosoftEdge.Stable* -AllUsers
+::Remove-AppXProvisionedPackage -Online -PackageName Microsoft.MicrosoftEdgeDevToolsClient* -AllUsers
+::Remove-AppXProvisionedPackage -Online -PackageName Microsoft.Edge.GameAssist* -AllUsers
+::Remove-AppXProvisionedPackage -Online -PackageName Microsoft.MicrosoftEdge.Stable* -AllUsers
+::del /s "c:\Users\Administrator\Desktop\Microsoft Edge.lnk" >nul 2>&1
 
 Remove-AppXProvisionedPackage -Online -PackageName Microsoft.OutlookForWindows* -AllUsers
 Remove-AppXProvisionedPackage -Online -PackageName Microsoft.Windows.NarratorQuickStart*
 Remove-AppXProvisionedPackage -Online -PackageName Microsoft.Windows.DevHome*
 Remove-AppXProvisionedPackage -Online -PackageName AppUp.IntelGraphicsExperience*
-dek /s "c:\Users\Administrator\Desktop\Microsoft Edge.lnk" >nul 2>&1
 
 title Installing Brave
 winget install -e --id Brave.Brave
