fd08f1746dbea704479bd5c1fecb365ff3a63c26
methanoid <methanoid@gmail.com>
2025-07-23 11:14:43 +0100

    Update WIN_Run_as_ADMIN.bat
    More Win11 tweaks and move stuff to Schneegans

--- WIN_Run_as_ADMIN.bat
+++ WIN_Run_as_ADMIN.bat
@@ -16,16 +16,14 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName" /v "Co
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "Hostname" /t REG_SZ /d %NUNAME% /f >nul
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v "NV Hostname" /t REG_SZ /d %NUNAME% /f >nul
 
-title Activation and Wallpaper
+title Windows Activation
 powershell.exe -ex bypass "irm https://get.activated.win | iex"
-powershell -ex bypass -Command "irm https://raw.githubusercontent.com/methanoid/setup/main/WIN_wallpaper.ps1 | iex" >nul
-
 label C: Win11 >nul
 
 title RegTweaks
-
 ::  Set PC network discoverable
 netsh advfirewall firewall set rule group="Network Discovery" new enable=Yes >nul
+&&&&&&&&&&&&&&&&&&&&&&ANOTHER HERE
 
 :: Remove Realtek Control Panel (will error if not present)
 reg delete HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run /v "RTHDVCPL" /f >nul
@@ -33,29 +31,12 @@ reg delete HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run /v "
 :: Auto Arrange Icons On
 reg add "HKCU\SOFTWARE\Microsoft\Windows\Shell\Bags\1\Desktop" /v FFLAGS /t REG_DWORD /d 1075839525 /f >nul
 
-:: Add Network Icon
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{F02C1A0D-BE21-4350-88B0-7367FC96EF3C}" /t REG_DWORD /d "0" /f >nul
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu" /v "{F02C1A0D-BE21-4350-88B0-7367FC96EF3C}" /t REG_DWORD /d "0" /f >nul
-
-:: Add This PC Icon
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d "0" /f >nul
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d "0" /f >nul
-
 :: Stop Explorer from showing external drives twice
 reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\DelegateFolders\{F5FB2C77-0E2F-4A16-A381-3E560C68BC83}" /f >nul
 reg delete "HKLM\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\DelegateFolders\{F5FB2C77-0E2F-4A16-A381-3E560C68BC83}" /f >nul
 
-:: Put Shortcut Arrow on Shortcuts back
-reg add "HKCR\IE.AssocFile.URL" /v "IsShortcut" /t REG_SZ /d "" /f >nul
-reg add "HKCR\InternetShortcut" /v "IsShortcut" /t REG_SZ /d "" /f >nul
-reg add "HKCR\lnkfile" /v "IsShortcut" /t REG_SZ /d "" /f >nul
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons" /v "29" /t REG_SZ /d "-" /f >nul
-
 :: Disable Taskbar Transparency (needed for NV 7 Series)
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize]" /v "EnableTransparency" /t REG_DWORD /d "0" /f >nul
-
-:: Removing a few bits
-PowerShell "Disable-WindowsOptionalFeature -FeatureName "WindowsMediaPlayer" -Online"
 
 title Power Planning
 :choice
@@ -80,24 +61,12 @@ REM Maybe add HP laptop utils ?
 :done
 
 
-:: This script is downloaded from Github by the AutoUnattend.XML file on the ISO
-@echo off
-title Windows Customization - Be patient
-
-
 :: ==INSTALLS========================================================================================================================
-
-"C:\Portable Apps\d3dx43.exe"
 
 :: Drivers (not needed if DISM updated)
 "C:\Portable Apps\Driver Booster\DriverBoosterPortable.exe"
 
-:: Now install Chocolatey
-title Installing Chocolatey
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-
 title Installing Brave
-:choco upgrade -y brave >nul
 winget install -e --id Brave.Brave
 del /s "c:\Users\%username%\Desktop\Brave.lnk" >nul 2>&1
 schtasks /delete /tn BraveUpdateTaskMachineCore /f >nul 2>&1
@@ -125,47 +94,36 @@ winget install -e --id ElectronicArts.EADesktop
 :no
 
 title Installing 7Zip
-:choco upgrade -y 7zip >nul
 winget install -e --id 7zip.7zip
 
 title Installing Notepad++
-:choco upgrade -y notepadplusplus.install >nul
 winget install -e --id Notepad++.Notepad++
 
 title Installing Redist Files
-:choco upgrade -y vcredist140 >nul
 winget install -e --id Microsoft.VCRedist.2015+.x64
 
 title Installing OnlyOffice
-:choco upgrade -y onlyoffice >nul
 winget install -e --id ONLYOFFICE.DesktopEditors
-net stop "OnlyOffice Update Service" >nul
-sc config "OnlyOffice Update Service" start= disabled >nul
 
 title Installing Putty
-:choco upgrade -y putty.install >nul
 winget install -e --id PuTTY.PuTTY
 
 title Installing SumatraPDF
-:choco upgrade -y sumatrapdf >nul
 winget install -e --id SumatraPDF.SumatraPDF
 del /s "c:\Users\Public\Desktop\SumatraPDF.lnk" >nul 2>&1
 
 title Installing ImgBurn
-:choco upgrade -y imgburn >nul
 winget install -e --id LIGHTNINGUK.ImgBurn
 del /s "c:\Users\%username%\Desktop\ImgBurn.lnk" >nul 2>&1
 del /s "c:\Users\Public\Desktop\ImgBurn.lnk" >nul 2>&1
 
 title Installing KLite Codecs
-:choco upgrade -y k-litecodecpack-standard >nul
 winget install -e --id CodecGuide.K-LiteCodecPack.Standard
 del /s "c:\Users\%username%\Desktop\MPC-HC x64.lnk" >nul 2>&1
 del /s "c:\Users\Public\Desktop\mpc-be.lnk" >nul 2>&1
 schtasks /delete /tn klcp_update /f >nul 2>&1
 
 title Installing Minecraft Launcher
-:choco upgrade -y prismLauncher >nul
 winget install -e --id PrismLauncher.PrismLauncher
 
 title Installing LockHunter
@@ -178,9 +136,6 @@ title Installing Hashtab
 winget install -e --id namazso.OpenHashTab
 
 title Installing Privado VPN
-:powershell -Command "Invoke-WebRequest https://privadovpn.com/apps/win/Setup_PrivadoVPN_latest.exe -OutFile c:\Privado.exe"
-:c:\Privado.exe /s
-:del /s "c:\Privado.exe" >nul 2>&1
 winget install -e --id PrivadoNetworksAG.PrivadoVPN
 :: Fix for VPN adding
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\PolicyAgent" /v "AssumeUDPEncapsulationContextOnSendRule" /t REG_DWORD /d "2" /f >nul      
@@ -205,12 +160,7 @@ REM VideoReDo.TVSuite.6.63.7.836
 del /s "c:\Portable Apps\VRD.exe" >nul 2>&1
 
 
-:: This script is downloaded from Github by the AutoUnattend.XML file on the ISO
-@echo off
-title Windows Customization - Be patient
-
 :: ==CLEANUPS========================================================================================================================
-
 
 title Some File Cleaning
 "C:\Portable Apps\BleachBit\bleachbit_console.exe" -c --preset >nul 2>&1        ::   MSVCR100 ERROR!!!!
@@ -222,26 +172,20 @@ title Some File Cleaning
 "C:\Portable Apps\Wise Disk Cleaner\WiseDiskCleaner.exe"
 move "C:\Portable Apps\Wise Disk Cleaner.lnk" "c:\Users\%username%\Desktop\" >nul 2>&1
 
+
 :: ==REBOOT==++======================================================================================================================
 
-:: Reboot
 echo "Rebooting now - enjoy!"
-PAUSE
 shutdown -r -t 5
 
 
-==REDUDNANT?=================================
-
+==REDUNDANT?=================================
 
 title Installing DirectX
 choco upgrade -y directx >nul
 
-:: LanmanWorkstation to Enable Connection to unRAID & Map Z drive
-:: reg add HKLM\Software\Policies\Microsoft\Windows\LanmanWorkstation /v AllowInsecureGuestAuth /t REG_DWORD /d "1" /f >nul
-:: net use z: \\UBERSERVER\data 2>nul
-:: taskkill /f >nul /im explorer.exe  2>nul 
-:: start c:\windows\explorer.exe  2>nul 
-
 :: Windows update check and update
 :: wuauclt /detectnow
 :: wuauclt /updatenow
+
+:: "C:\Portable Apps\d3dx43.exe"
