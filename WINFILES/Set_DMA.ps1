65ba5c72cb7cd7f9a5b33f22c5ccb5a69b865be5
methanoid <methanoid@gmail.com>
2025-09-07 13:15:00 +0100

    Update Set_DMA.ps1

--- WINFILES/Set_DMA.ps1
+++ WINFILES/Set_DMA.ps1
@@ -1,6 +1,10 @@
 $filePath = "C:\Windows\System32\IntegratedServicesRegionPolicySet.json"
 takeown /F $filePath /A
-icacls $filePath /grant "$currentUser:F" /T
+icacls $filePath /grant "Administrator:F" /T
 $content = Get-Content -Path $filePath -Raw
 $updatedContent = $content -replace '"YT"', '"YT" , "GB"'
 Set-Content -Path $filePath -Value $updatedContent -Force
+
+
+takeown /f "C:\Windows\System32\IntegratedServicesRegionPolicySet.json"
+icacls "C:\Windows\System32\IntegratedServicesRegionPolicySet.json" /grant "Username:(F)"
