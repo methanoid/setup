22c4051c9a0bbabb4b2365d702ac726e86340bdc
methanoid <methanoid@gmail.com>
2025-09-09 10:45:35 +0100

    Update Die_Edge_Die.ps1

--- WINFILES/Die_Edge_Die.ps1
+++ WINFILES/Die_Edge_Die.ps1
@@ -1,1 +1,1 @@
-iex "&{$(irm https://cdn.jsdelivr.net/gh/he3als/EdgeRemover@main/get.ps1)} -UninstallEdge -RemoveEdgeData -InstallWebView"
+iex "&{$(irm https://cdn.jsdelivr.net/gh/he3als/EdgeRemover@main/get.ps1)} -UninstallEdge -RemoveEdgeData -UninstallWebView"
