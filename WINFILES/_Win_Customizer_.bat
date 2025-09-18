990d0327edfca23cf391e12c54f1beb102d1271b
methanoid <methanoid@gmail.com>
2025-09-18 14:38:26 +0100

    Update _Win_Customizer_.bat

--- WINFILES/_Win_Customizer_.bat
+++ WINFILES/_Win_Customizer_.bat
@@ -1,6 +1,6 @@
 :: == WINDOWS CUSTOMIZER SCRIPT for WINDOWS 10 & 11 LTSC ========== $OEM$ inc Customizer & EdgeDie on W10 ============================
 
-REM echo: adds a blank line to screen
+REM echo: adds a blank line to screen        hide meet now icon      hide search bar which reappears after update
 
 :: ==TWEAKS===========================================================================================================================
 @echo off
