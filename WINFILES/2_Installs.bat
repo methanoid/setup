812fc31447d59986d3ecd72070c53b88bb08953d
methanoid <methanoid@gmail.com>
2025-10-18 20:10:53 +0100

    Update 2_Installs.bat
    Slim CCleaner

--- WINFILES/2_Installs.bat
+++ WINFILES/2_Installs.bat
@@ -129,7 +129,7 @@ powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/methano
 echo:
 
 title Installing CCleaner
-winget install -e -h --id Piriform.CCleaner
+winget install -e -h --id Piriform.CCleaner.slim
 del /s "c:\Users\Public\Desktop\CCleaner.lnk" >nul 2>&1
 powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/methanoid/setup/main/WINFILES/ccleaner.ini -OutFile 'C:\Program Files\CCleaner\ccleaner.ini'"
 powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/methanoid/setup/main/WINFILES/winapp2.ini -OutFile 'C:\Program Files\CCleaner\winapp2.ini'"
